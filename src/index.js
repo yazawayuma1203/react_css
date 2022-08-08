a5f8a546c09c927d6d45326b8bed8250d28171f8
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-08 21:42:57 +0900

    17対応 reactを17に変更

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
